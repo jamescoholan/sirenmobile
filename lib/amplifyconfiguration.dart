7d7d7d3a02381e14a6dec580c600f5daa1ff0eb3
James Coholan <jamescoholan@gmail.com>
2021-05-04 10:54:38 -0700

    commit on 'main'

---
+++ lib/amplifyconfiguration.dart
@@ -0,0 +1,17 @@
+const amplifyconfig = ''' {
+    "UserAgent": "aws-amplify-cli/2.0",
+    "Version": "1.0",
+    "api": {
+        "plugins": {
+            "awsAPIPlugin": {
+                "sirenmobile": {
+                    "endpointType": "GraphQL",
+                    "endpoint": "https://m33hac2jabcq3nvmuftay5atwi.appsync-api.us-west-2.amazonaws.com/graphql",
+                    "region": "us-west-2",
+                    "authorizationType": "API_KEY",
+                    "apiKey": "da2-c653isjqfrbhrj7px45ds5iwwe"
+                }
+            }
+        }
+    }
+}''';
